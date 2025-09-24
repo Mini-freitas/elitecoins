341c4210b73495226cb13161294ce2ded803443f
Mini-freitas <mateusvicentedefreitas17@gmail.com>
2025-09-24 16:30:06 -0300

    mudança no css ajuste responsividade admin

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL, // Vite usa import.meta.env
+  baseURL: import.meta.env.VITE_API_URL, // Vite usa import.meta.env
 });
 
 export default api;
